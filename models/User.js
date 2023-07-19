059b00c7968cc2285eae4b8b376d61024e3e3857
junyeoplee <jyeop920@gmail.com>
2023-07-19 22:39:03 +0900

    .

--- models/User.js
+++ models/User.js
@@ -8,19 +8,19 @@ const userSchma = mongoose.Schema({
     name: {
         type: String,
         maxlength: 50, //최대 글자수
-        minlength: 5,
+        minlength: 5
     },
     //이메일
     email: {
         type: String,
         trim: true, //이메일을 입력할 때 띄어쓰기가 있으면 띄어쓴 부분 없애줌
-        unique: 1, //이메일 중복 안되게 설정
+        unique: 1 //이메일 중복 안되게 설정
     },
     //비번
     password: {
         type: String,
         maxlength: 15,
-        minlength: 8,//최소 글자수
+        minlength: 8//최소 글자수
     },
     //role의 숫자여부에 따라 관리자인지 일반 회원인지 판별 할 수 있게 설정
     role: {
--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const app = express();
 const PORT = 7070;
+const bodyParser = require("body-parser");
+//유저 모델을 가져옴
+const { User } = require("../models/User.js");
+
+//클라이언트의 req 를 json 형태로 해석 하도록 도와줌
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const db = "mongodb+srv://jyeop920:toddlf0826@cluster0.mvqy3yr.mongodb.net/?retryWrites=true&w=majority";
 const mongoose = require("mongoose");
 mongoose.connect(db , {
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    useCreateIndex:true,
-    useFindAndModify:false
+    //useNewUrlParser:true,
+    //useUnifiedTopology:true,
+    //useCreateIndex:true,
+    //useFindAndModify:false
 }).then((req , res) => {
     console.log("MongoDB Connected!");
 }).catch((error) => {
@@ -16,9 +23,26 @@ mongoose.connect(db , {
 });
 
 app.listen(PORT , (req , res) => {
-    console.log('hello world');
+    console.log("hello world");
 });
 
-app.get('/' , (req , res) => {
-    res.send('하이하잉');
+app.get("/" , (req , res) => {
+    res.send("하이하잉");
+});
+
+app.post("/register" , (req , res) => {
+    //인스턴스 객체 생성 후 클라이언트 요청을 담는다
+    const user = new User(req.body);
+    //정보를 db에 보내준다. 이때 , 성공하거나 에러가 나면 메세지를 json 형식으로 보내준다.
+    //mongoDB 메서드, user모델에 저장
+    //mongoose 6버전 부터는 save 에 콜백함수를 지원하지 않아 아래와 같이 코드 작성
+    user.save()
+    .then(()=>{
+    res.status(200).json({
+        success: true
+    });
+    })
+    .catch((error)=>{
+    res.json({ success: false, error });
+    });
 });
