fe7465c497fd3e37a20db52d2e3a49d6e834086c
junyeoplee <jyeop920@gmail.com>
2023-07-31 01:40:35 +0900

    [Feat , Error] : redux-persist 로 로그인 유저정보 session에 저장
    브라우저 새로 고침해도 유저정보가 초기화 되지 않도록 persist , logger를 install 해서 코드 작성 그런데 getDefaultMiddleware에 에러가 있어서 해결 해야함 세션에 등록까진 잘됨

--- client/src/index.js
+++ client/src/index.js
@@ -6,19 +6,21 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import { persistStore} from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
 
-
-
-
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading ={null} persistor ={persistor}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,14 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore , combineReducers , getDefaultMiddleware } from "@reduxjs/toolkit";
 import userSlice from "./slice/userSlice";
+import storageSession from "redux-persist/lib/storage/session";
+import { persistReducer } from "redux-persist";
+import logger from "redux-logger";
 
 
+const persistConfig = {
+    key: "user",
+    storage: storageSession,
+    whitelist: ["user"]
+}
 
+const rootReducer = combineReducers({
+    user: userSlice.reducer
+})
 
-export default configureStore({
-    reducer: { 
-        user : userSlice.reducer
-    }
+const persistedReducer =  persistReducer(persistConfig, rootReducer);
+
+const store =  configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 });
+
+export default store;
 
 
 
