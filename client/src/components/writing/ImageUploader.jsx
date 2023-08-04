91a646b971c3c32ae15b81231f49ffc18e72170d
junyeoplee <jyeop920@gmail.com>
2023-08-04 10:42:44 +0900

    [remove Add] : ckEditor 결국 또 실패 후 다른방식 시도
    대표 사진만 이미지 업로드 할 수 있고
    나머지는 글 내용으로만 게시판 만들 예정

---
+++ client/src/components/writing/ImageUploader.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCamera } from "@fortawesome/free-solid-svg-icons";
+
+const ImageUploader = () => {
+    return (
+        <>
+        <div className = "image-upload__container">
+        <input
+        type="file"
+        accept="image/*"
+        style={{ display: "none" }}/>
+        <FontAwesomeIcon
+        className ="camera-icon"
+        icon ={ faCamera }
+        size = "5x"/>
+        <div className="img-wrapper">
+        <img src="" />
+        </div>
+        </div>
+        </>
+    );
+};
+
+export default ImageUploader;
--- client/src/pages/WritingPage.jsx
+++ client/src/pages/WritingPage.jsx
@@ -1,53 +1,48 @@
-import React from 'react';
+import React ,{ useState }from 'react';
 import Navbar from "../components/navbar/Navbar";
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import ScrollToTopButton from "../components/ScrollToTopButton";
+import ImageUploader from "../components/writing/ImageUploader";
 import * as styled from "../styles/styledComponents";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const WritingPage = () => {
+    const navigate = useNavigate();
+
     return (
         <>
         <Navbar/>
         <div className ="editor-container">
-            <input className ="editor-title" type="text" placeholder = "제목" />
+            <form 
+            className = "editor-form"
+            onSubmit = {(e) => {e.preventDefault()}}>
+                    <ImageUploader/>
+                    <div className ="writing-container">
+                    <input 
+                    className ="editor-title" 
+                    type="text" 
+                    placeholder = "제목" />  
+                    <div className ="content-container">
+                    <textarea
+                    className="content"
+                    placeholder="내용을 입력하세요"/>            
+                    </div>
+                    <div className ="writing-button__container">
+                    <styled.DeleteButton
+                    className ="writing-button delete-btn"
+                    onClick={() => navigate(-1, { replace: true })}>
+                        취소
+                    </styled.DeleteButton>
 
-            <CKEditor
-            className ="editor"
-            editor={ ClassicEditor }
-            config={{
-                placeholder:"내용을 입력하세요!",
-            }}
-            data=""
-            onReady={ editor => {
-                // You can store the "editor" and use when it is needed.
-                console.log( 'Editor is ready to use!', editor );
-            } }
-            onChange={ ( event, editor ) => {
-                const data = editor.getData();
-                console.log( { event, editor, data } );
-            } }
-            onBlur={ ( event, editor ) => {
-                console.log( 'Blur.', editor );
-            } }
-            onFocus={ ( event, editor ) => {
-                console.log( 'Focus.', editor );
-            } }
-            />
-
-            <div className ="writing-button__container">
-                <styled.DeleteButton
-                className ="writing-button delete-btn">
-                    취소
-                </styled.DeleteButton>
-
-                <styled.SubmitButton
-                className ="writing-button default-btn">
-                    등록
-                </styled.SubmitButton>
-            </div>
+                    <styled.SubmitButton
+                    type = "submit"
+                    className ="writing-button default-btn">
+                        등록
+                    </styled.SubmitButton>
+                </div>
+                </div>
+            </form>
         </div>
-
         <ScrollToTopButton/>
         </>
     );
